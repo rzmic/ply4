b20455ed10a6323eabdc2b148411b350048e0ff2
rzmic <rangga@zn24r.onmicrosoft.com>
2024-07-13 07:00:41 +0700

    Add files via upload

---
+++ proxy.js
@@ -0,0 +1,17 @@
+var proxy1 =''; 
+var proxy2 = ''; 
+
+const proxies = [
+  'https://production.m4-math-2250-sdf659899fff-8h5sdf87sdf-sd5fsdfsd12-sdf556sfdsd.hdtzs8r9ph.workers.dev/',
+  'https://production.m4-kasd8asdasd-av7fggg-hivd7-jsfd7v-sdf7687787xcv-vfjuius-uviuf.r2024-822.workers.dev/',
+  'https://production.m4-unit2-137f123132125665232365898362asd952323333ere32222336585.2eca2e9bx5.workers.dev/',
+  'https://production.m4-field-9780-asd6865-65a54sd-5asdas-45asda-asd54445dfs-hhfg87j.m4-31e.workers.dev/',
+  'https://production.m4-breeze-cc3089sdj-a98sduna0-98asjda908734j9-alkkjxa987-asdlkm.p75zhjx6rt.workers.dev/',
+  'https://production.m4-meadow-858e-8asdj09asdjn9087890-asd890as-kcsec987dc-ckd98-cx.f0o8q1z04c.workers.dev/'
+];
+
+// Select a random proxy
+const randomIndex = Math.floor(Math.random() * proxies.length);
+const proxy3 = proxies[randomIndex];
+
+var proxy4 = ''; 
