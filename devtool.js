1fba8ab8cb223b2f705f5ee0bf696f5cb617d64a
rzmic <rangga@zn24r.onmicrosoft.com>
2024-08-02 03:32:00 +0700

    Create devtool.js

---
+++ devtool.js
@@ -0,0 +1,56 @@
+(function () {
+        
+            function disableCopyPaste(element) {
+                if (element) {
+                    ["mousedown", "selectstart", "copy", "cut", "paste", "dragstart", "drop"].forEach(function (event) {
+                        element.addEventListener(event, function (e) {
+                            e.preventDefault();
+                            return false;
+                        });
+                    });
+                }
+            }
+        
+            function disableRightClick(event) {
+                event.preventDefault();
+            }
+        
+            function disableShortcuts(event) {
+                const forbiddenKeys = [16, 67, 86, 85, 117, 123, 73, 82];
+                if (forbiddenKeys.includes(event.keyCode)) {
+                    event.preventDefault();
+                }
+            }
+        
+            function detectDevTool() {
+                const threshold = 100; // milliseconds
+                const start = performance.now();
+                eval("debugger");
+                const end = performance.now();
+                if (end - start > threshold) {
+                    window.location.replace("https://ply4.com/hello.html");
+                }
+            }
+        
+            function attachHandlers() {
+                window.addEventListener('resize', detectDevTool);
+                window.addEventListener('mousemove', detectDevTool);
+                window.addEventListener('focus', detectDevTool);
+                window.addEventListener('blur', detectDevTool);
+                window.addEventListener('contextmenu', disableRightClick);
+                window.addEventListener('keydown', disableShortcuts);
+                disableCopyPaste(document.body);
+                disableCopyPaste(document.querySelector("input[type='text']"));
+            }
+        
+            if (document.readyState === "complete" || document.readyState === "interactive") {
+                detectDevTool();
+                attachHandlers();
+            } else {
+                document.addEventListener('DOMContentLoaded', function () {
+                    detectDevTool();
+                    attachHandlers();
+                });
+            }
+        
+        })();
