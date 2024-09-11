d6e002b3550f52fc54fa8aa8beb9122257743d8d
rzmic <rangga@zn24r.onmicrosoft.com>
2024-09-11 17:07:32 +0700

    Create proxies.js

---
+++ proxies.js
@@ -0,0 +1,18 @@
+var proxy2 =''; 
+var proxy3 = ''; 
+var proxy4 = ''; 
+
+const proxies = [
+  'https://production.floral-heart-4712-65413-5611399-6536-9862568-fghr9-65fgh6tfghhh.5ebcurz0je.workers.dev/',
+  'https://production.m4-math-2250-sdf659899fff-8h5sdf87sdf-sd5fsdfsd12-sdf556sfdsd.hdtzs8r9ph.workers.dev/',
+  'https://production.m4-kasd8asdasd-av7fggg-hivd7-jsfd7v-sdf7687787xcv-vfjuius-uviuf.r2024-822.workers.dev/',
+  'https://production.m4-unit2-137f123132125665232365898362asd952323333ere32222336585.2eca2e9bx5.workers.dev/',
+  'https://production.m4-field-9780-asd6865-65a54sd-5asdas-45asda-asd54445dfs-hhfg87j.m4-31e.workers.dev/',
+  'https://production.m4-breeze-cc3089sdj-a98sduna0-98asjda908734j9-alkkjxa987-asdlkm.p75zhjx6rt.workers.dev/',
+  'https://production.m4-meadow-858e-8asdj09asdjn9087890-asd890as-kcsec987dc-ckd98-cx.f0o8q1z04c.workers.dev/',
+  'https://production.spring-truth-2ce098asd-90asdasdloov-8878jcd-89zxckd-9zcjkdkuklk.ply4.workers.dev/'
+];
+
+// Select a random proxy
+const randomIndex = Math.floor(Math.random() * proxies.length);
+const proxy1 = proxies[randomIndex];
