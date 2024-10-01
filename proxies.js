d75496f8e9b48fdfacc87e57f868d6ac4516f67f
rzmic <rangga@zn24r.onmicrosoft.com>
2024-10-01 15:31:21 +0700

    Update proxies.js

--- proxies.js
+++ proxies.js
@@ -20,8 +20,22 @@ const proxies2 = [
   'https://wandering-snowflake-d20av4we5vwd45bn6456dtg-54656ftgd546dku-657.gtxskgkai.workers.dev/'  
 ];
 
+const proxies3 = [
+  'https://solitary-disk-b362asjdhk0-98asmdjas9087nm234m0923-89734jd98834s.rjoaqvpai.workers.dev/',
+  'https://cool-recipe-ab49dsf-84357893475-sdf453-fvsd3453-xgfnh546-dfg435.wtrabwpai.workers.dev/',
+  'https://black-leaf-f7ce23sdko-4989xcj2389-sdkkiuifdj8-934jk983-349dskjj.stafzmixh.workers.dev/',
+  'https://dry-glade-e770fgbdf-546fdg567gfh-7890fgh34-gfh567bhgfgh56-dg456.adjefnixh.workers.dev/',
+  'https://purple-sun-65e6fvs4df-090909090-gfh456fbg-34245435635gdgyuyu-34.zyzafidoj1l3.workers.dev/',
+  'https://holy-rice-e193dfg456fgvfg567567-546fdg456-567fgdf6fghjl-lkdfg5d.kog0h1f0nin2.workers.dev/',
+  'https://falling-sun-a478fgh546fdgdf6-90768fgh567-657fghfd65-fdg56fgdf-5.047ssggwcg.workers.dev/',
+  'https://winter-morning-3717laksd-7asd8hjqwe-3243243-asdasd324-sdf34-gfd.xwd4dtef.workers.dev/'
+];
+
 // Select a random proxy
 const randomIndex1 = Math.floor(Math.random() * proxies.length);
 const randomIndex2 = Math.floor(Math.random() * proxies2.length);
+const randomIndex3 = Math.floor(Math.random() * proxies3.length);
+
 const proxy1 = proxies[randomIndex1];
 const proxy2 = proxies2[randomIndex2];
+const proxy3 = proxies3[randomIndex3];
