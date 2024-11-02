446fe8021bd9685958c59a64cb2c47f4ac13f5b2
rzmic <rangga@zn24r.onmicrosoft.com>
2024-11-02 17:30:01 +0700

    Update proxies.js

--- proxies.js
+++ proxies.js
@@ -10,35 +10,45 @@ const proxies = [
     'https://shiny-mud-6afbas99234656-9028346hfdskfjsd-3457645hdjie7645-0hds.priakesepianbutuhngewesoalnyangacengbae.workers.dev/',
     'https://semua-terserah-padamu-akuhanya-manusia-biasa-yangpunya-kontolll.butuhmemeksegar.workers.dev/',
     'https://ohkenapa-memekitu-enakbanget-apalagidijilat-disedot-dicelup2in.memekbasah.workers.dev/'
-];
+  ];
+  
+  const proxies2 = [
+    'https://odd-bird-0dd9-sg435-b4-5bhfgdhfdg456t-hbfgdh-5hfgdh5556dj-678j7.agemueorn.workers.dev/',
+    'https://red-salad-402a324-dsfkl98324-mcds87sdn-987dfne-9834kj8-isabf563.zzirggwjg.workers.dev/',
+    'https://green-wave-9adc8-sf876sf-sdf90900sdfkl-bmnkji93456-fio4590-kfds.jiswciwjg.workers.dev/',
+    'https://rapid-lab-234fasd-oasdaos933-naid76823-mjaksduj-3489n-83asd0099.bwxcsiwjg.workers.dev/',
+    'https://still-rice-580987jhjks-dkiuiy87349-klhns8834j-injsi79jds-93j-k9.y9p4ha1c.workers.dev/',
+    'https://red-butterfly-79adjnkj8hjjkjk0098-89jlkj897hkjh786-asasde555hjg.ahiudjslo.workers.dev/',
+    'https://broad-sunset-53f2-u8jkhjh76ggjhjyt5454657657-kjjk87-980knj89-90.agenzbzow.workers.dev/',
+    'https://wandering-snowflake-d20av4we5vwd45bn6456dtg-54656ftgd546dku-657.gtxskgkai.workers.dev/'  
+  ];
+  
+  const proxies3 = [
+    'https://solitary-disk-b362asjdhk0-98asmdjas9087nm234m0923-89734jd98834s.rjoaqvpai.workers.dev/',
+    'https://cool-recipe-ab49dsf-84357893475-sdf453-fvsd3453-xgfnh546-dfg435.wtrabwpai.workers.dev/',
+    'https://black-leaf-f7ce23sdko-4989xcj2389-sdkkiuifdj8-934jk983-349dskjj.stafzmixh.workers.dev/',
+    'https://dry-glade-e770fgbdf-546fdg567gfh-7890fgh34-gfh567bhgfgh56-dg456.adjefnixh.workers.dev/',
+    'https://purple-sun-65e6fvs4df-090909090-gfh456fbg-34245435635gdgyuyu-34.zyzafidoj1l3.workers.dev/',
+    'https://holy-rice-e193dfg456fgvfg567567-546fdg456-567fgdf6fghjl-lkdfg5d.kog0h1f0nin2.workers.dev/',
+    'https://falling-sun-a478fgh546fdgdf6-90768fgh567-657fghfd65-fdg56fgdf-5.047ssggwcg.workers.dev/',
+    'https://winter-morning-3717laksd-7asd8hjqwe-3243243-asdasd324-sdf34-gfd.xwd4dtef.workers.dev/'
+  ];
+  
+  const pornproxies = [
+    'https://5656se4cd54e5c454-http-dsjke343nc87fdvds-https-8475mjjkcf8945co.5a9hz.workers.dev/',
+    'https://fancy-98sdjjk983-com-http-ilsdkds38780-https-7u48du99jjc-net-ja.du3jts.workers.dev/',
+    'https://xx90xxx-http-eieur-com-jjr653jnc8734nj99-https-iurj887-net-uah8.az84pzji.workers.dev/',
+    'https://ksaud8734-xhttp-ueiorj8392486-com-iemmf998-https-38987jjmie-net.akwoxkb.workers.dev/',
+    'https://ydsayu673-xhttp-ioia773-http-klasde783-com-yu7389m-https-873jhc.m41qsv.workers.dev/'
+  ]
 
-const proxies2 = [
-  'https://odd-bird-0dd9-sg435-b4-5bhfgdhfdg456t-hbfgdh-5hfgdh5556dj-678j7.agemueorn.workers.dev/',
-  'https://red-salad-402a324-dsfkl98324-mcds87sdn-987dfne-9834kj8-isabf563.zzirggwjg.workers.dev/',
-  'https://green-wave-9adc8-sf876sf-sdf90900sdfkl-bmnkji93456-fio4590-kfds.jiswciwjg.workers.dev/',
-  'https://rapid-lab-234fasd-oasdaos933-naid76823-mjaksduj-3489n-83asd0099.bwxcsiwjg.workers.dev/',
-  'https://still-rice-580987jhjks-dkiuiy87349-klhns8834j-injsi79jds-93j-k9.y9p4ha1c.workers.dev/',
-  'https://red-butterfly-79adjnkj8hjjkjk0098-89jlkj897hkjh786-asasde555hjg.ahiudjslo.workers.dev/',
-  'https://broad-sunset-53f2-u8jkhjh76ggjhjyt5454657657-kjjk87-980knj89-90.agenzbzow.workers.dev/',
-  'https://wandering-snowflake-d20av4we5vwd45bn6456dtg-54656ftgd546dku-657.gtxskgkai.workers.dev/'  
-];
-
-const proxies3 = [
-  'https://solitary-disk-b362asjdhk0-98asmdjas9087nm234m0923-89734jd98834s.rjoaqvpai.workers.dev/',
-  'https://cool-recipe-ab49dsf-84357893475-sdf453-fvsd3453-xgfnh546-dfg435.wtrabwpai.workers.dev/',
-  'https://black-leaf-f7ce23sdko-4989xcj2389-sdkkiuifdj8-934jk983-349dskjj.stafzmixh.workers.dev/',
-  'https://dry-glade-e770fgbdf-546fdg567gfh-7890fgh34-gfh567bhgfgh56-dg456.adjefnixh.workers.dev/',
-  'https://purple-sun-65e6fvs4df-090909090-gfh456fbg-34245435635gdgyuyu-34.zyzafidoj1l3.workers.dev/',
-  'https://holy-rice-e193dfg456fgvfg567567-546fdg456-567fgdf6fghjl-lkdfg5d.kog0h1f0nin2.workers.dev/',
-  'https://falling-sun-a478fgh546fdgdf6-90768fgh567-657fghfd65-fdg56fgdf-5.047ssggwcg.workers.dev/',
-  'https://winter-morning-3717laksd-7asd8hjqwe-3243243-asdasd324-sdf34-gfd.xwd4dtef.workers.dev/'
-];
-
-// Select a random proxy
-const randomIndex1 = Math.floor(Math.random() * proxies.length);
-const randomIndex2 = Math.floor(Math.random() * proxies2.length);
-const randomIndex3 = Math.floor(Math.random() * proxies3.length);
-
-const proxy1 = proxies[randomIndex1];
-const proxy2 = proxies2[randomIndex2];
-const proxy3 = proxies3[randomIndex3];
+  // Select a random proxy
+  const randomIndex1 = Math.floor(Math.random() * proxies.length);
+  const randomIndex2 = Math.floor(Math.random() * proxies2.length);
+  const randomIndex3 = Math.floor(Math.random() * proxies3.length);
+  const randomIndex18 = Math.floor(Math.random() * pornproxies.length);
+  
+  const proxy1 = proxies[randomIndex1];
+  const proxy2 = proxies2[randomIndex2];
+  const proxy3 = proxies3[randomIndex3];
+  const proxy18 = pornproxies[randomIndex18];
